a5992ec4c4d0a7cd103859e8ed95050564f381a4
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-12-16 00:13:59 +0800

    feat: add part13 13.1-13.4 solution

---
+++ commands.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author TEXT,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes INT DEFAULT 0,
+    date TIME
+);
+
+INSERT INTO blogs (author, url, title, likes) VALUES ('Axel', 'sey.com', 'Book ko', 23);
+INSERT INTO blogs (author, url, title, likes) VALUES ('Elknirk', 'krinkle.com', 'Book ni Elkrnirk', 1);
